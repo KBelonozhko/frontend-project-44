91adae4de0c9b63f441f678fb5126dd7d51eea8e
KBelonozhko <belonozhecka@gmail.com>
2022-10-19 23:25:49 +0300

    refactor game-even.js

--- games/game-even.js
+++ games/game-even.js
@@ -1,13 +1,15 @@
 import controlsGame from '../src/index.js';
 
+const isEven = (number) => number % 2 === 0;
+
+const startEvenRound = () => {
+  const randomNumber = Math.round(Math.random() * 100);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, correctAnswer];
+};
+
 const startGame = () => {
   const gameEvenRules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const startEvenRound = () => {
-    const generateRandomNumder = () => Math.round(Math.random() * 100);
-    const randomNumber = generateRandomNumder();
-    const checkCorrectAnswer = () => (randomNumber % 2 === 0 ? 'yes' : 'no');
-    return [randomNumber, checkCorrectAnswer()];
-  };
   controlsGame(gameEvenRules, startEvenRound);
 };
 
