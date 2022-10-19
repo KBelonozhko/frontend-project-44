6628f1c042475081d3feebbe211d139e03cf67e8
KBelonozhko <belonozhecka@gmail.com>
2022-10-19 23:24:17 +0300

    rename index.js constant

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import readlineSync from 'readline-sync';
 import meetPlayer from './cli.js';
 
-const controlsGame = (rules, gameRound) => {
+const controlsGame = (rules, startGameRound) => {
   console.log('Welcome to the Brain Games!');
   const playerName = meetPlayer();
   console.log(rules);
   for (let i = 0; i < 3; i += 1) {
-    const questionAndCorrectAnswer = gameRound();
+    const questionAndCorrectAnswer = startGameRound();
     console.log(`Question: ${questionAndCorrectAnswer[0]}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === questionAndCorrectAnswer[1]) {
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer === questionAndCorrectAnswer[1]) {
       console.log('Correct!');
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionAndCorrectAnswer[1]}'.\nLet's try again, ${playerName}!`);
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${questionAndCorrectAnswer[1]}'.\nLet's try again, ${playerName}!`);
       return;
     }
   }
