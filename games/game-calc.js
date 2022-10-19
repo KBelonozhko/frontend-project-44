10099555085f343c61cbf62dd3e5dde0d029a7c2
KBelonozhko <belonozhecka@gmail.com>
2022-10-19 23:16:44 +0300

    refactor game-calc.js

--- games/game-calc.js
+++ games/game-calc.js
@@ -1,31 +1,29 @@
 import controlsGame from '../src/index.js';
 
+const startCalcRound = () => {
+  const randomNumber1 = Math.round(Math.random() * 100);
+  const randomNumber2 = Math.round(Math.random() * 100);
+  const operators = ['+', '-', '*'];
+  const operatorIndex = Math.floor(Math.random() * operators.length);
+  const randomOperator = operators[operatorIndex];
+  const randomCalc = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+  const getCalcResult = () => {
+    switch (randomOperator) {
+      case '+':
+        return randomNumber1 + randomNumber2;
+      case '-':
+        return randomNumber1 - randomNumber2;
+      case '*':
+        return randomNumber1 * randomNumber2;
+      default:
+        return null;
+    }
+  };
+  return [randomCalc, `${getCalcResult()}`];
+};
+
 const startGame = () => {
   const gameCalcRules = 'What is the result of the expression?';
-  const startCalcRound = () => {
-    const generateRandomNumder1 = () => Math.round(Math.random() * 100);
-    const generateRandomNumder2 = () => Math.round(Math.random() * 100);
-    const randomNumber1 = generateRandomNumder1();
-    const randomNumber2 = generateRandomNumder2();
-    const operators = ['+', '-', '*'];
-    const operatorIndex = Math.floor(Math.random() * operators.length);
-    const randomOperator = operators[operatorIndex];
-    const randomCalc = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
-    const getCalcResult = () => {
-      switch (randomOperator) {
-        case '+':
-          return randomNumber1 + randomNumber2;
-        case '-':
-          return randomNumber1 - randomNumber2;
-        case '*':
-          return randomNumber1 * randomNumber2;
-        default:
-          // do nothing
-      }
-      return null;
-    };
-    return [randomCalc, `${getCalcResult()}`];
-  };
   controlsGame(gameCalcRules, startCalcRound);
 };
 
