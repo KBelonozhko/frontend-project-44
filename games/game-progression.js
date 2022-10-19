43111400bb50f43bc2a20a73be05b951cb14ee4d
KBelonozhko <belonozhecka@gmail.com>
2022-10-19 23:29:40 +0300

    refactor game-progression.js

--- games/game-progression.js
+++ games/game-progression.js
@@ -1,20 +1,21 @@
 import controlsGame from '../src/index.js';
 
+const startProgressionRound = () => {
+  const initialElement = Math.round(Math.random() * 100);
+  const progressionStep = Math.round(Math.random() * 10);
+  const progression = [initialElement];
+  for (let i = 0; i < 9; i += 1) {
+    const item = progression[i] + progressionStep;
+    progression.push(item);
+  }
+  const hiddenPosition = Math.floor(Math.random() * 10);
+  const hiddenElement = progression[hiddenPosition];
+  progression[hiddenPosition] = '..';
+  return [progression.join(' '), `${hiddenElement}`];
+};
+
 const startGame = () => {
   const gameProgressionRules = 'What number is missing in the progression?';
-  const startProgressionRound = () => {
-    const initialElement = Math.round(Math.random() * 100);
-    const progressionStep = Math.round(Math.random() * 10);
-    const progression = [initialElement];
-    for (let i = 0; i < 9; i += 1) {
-      const item = progression[i] + progressionStep;
-      progression.push(item);
-    }
-    const hiddenPosition = Math.floor(Math.random() * 10);
-    const hiddenElement = progression[hiddenPosition];
-    progression[hiddenPosition] = '..';
-    return [progression.join(' '), `${hiddenElement}`];
-  };
   controlsGame(gameProgressionRules, startProgressionRound);
 };
 
