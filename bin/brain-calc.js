aa50846ce8159ae6cf544d24bdd07d420a7f92c3
KBelonozhko <belonozhecka@gmail.com>
2022-10-20 20:52:29 +0300

    refactor bin/

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import startGame from '../games/game-calc.js';
+import startGame from '../src/games/game-calc.js';
 
 startGame();
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import startGame from '../games/game-prime.js';
+import startGame from '../src/games/game-prime.js';
 
 startGame();
