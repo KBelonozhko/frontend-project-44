d5a2b4fb2c3fb9b425efb27e4f12f411fc61bd76
KBelonozhko <belonozhecka@gmail.com>
2022-10-22 13:37:15 +0300

    refactor game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,18 +1,23 @@
 import controlGame from '../index.js';
 import getRandomNumber from '../helper-function.js';
 
+const getProgression = (array, arrayLength, arrayStep) => {
+  for (let i = 0; array.length < arrayLength; i += 1) {
+    array.push(array[i] + arrayStep);
+  }
+  return array;
+};
+
 const startProgressionRound = () => {
   const initialElement = getRandomNumber(0, 100);
   const progressionStep = getRandomNumber(0, 10);
   const progression = [initialElement];
-  for (let i = 0; i < 9; i += 1) {
-    const item = progression[i] + progressionStep;
-    progression.push(item);
-  }
+  const progressionLength = 10;
+  const resultingProgression = getProgression(progression, progressionLength, progressionStep);
   const hiddenPosition = getRandomNumber(0, 10);
-  const hiddenElement = progression[hiddenPosition];
-  progression[hiddenPosition] = '..';
-  return [progression.join(' '), `${hiddenElement}`];
+  const hiddenElement = resultingProgression[hiddenPosition];
+  resultingProgression[hiddenPosition] = '..';
+  return [resultingProgression.join(' '), `${hiddenElement}`];
 };
 
 const startGame = () => {
