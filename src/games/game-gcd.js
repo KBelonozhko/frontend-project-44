c166365e2f5a66021dc4e6c99a7ed457a83af4db
KBelonozhko <belonozhecka@gmail.com>
2022-10-22 11:52:31 +0300

    refactor game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,21 +1,25 @@
 import controlsGame from '../index.js';
 import getRandomNumber from '../helper-function.js';
 
-const startGcdRound = () => {
-  let randomNumber1 = getRandomNumber(0, 100);
-  let randomNumber2 = getRandomNumber(0, 100);
-  const givenNumbers = `${randomNumber1} ${randomNumber2}`;
-  const findsGcd = () => {
-    while (randomNumber1 !== randomNumber2) {
-      if (randomNumber1 > randomNumber2) {
-        randomNumber1 -= randomNumber2;
-      } else {
-        randomNumber2 -= randomNumber1;
-      }
+const findsGcd = (number1, number2) => {
+  let num1 = number1;
+  let num2 = number2;
+  while (num1 !== num2) {
+    if (num1 > num2) {
+      num1 -= num2;
+    } else {
+      num2 -= num1;
     }
-    return randomNumber1;
-  };
-  return [givenNumbers, `${findsGcd()}`];
+  }
+  return num1;
+};
+
+const startGcdRound = () => {
+  const randomNumber1 = getRandomNumber(0, 100);
+  const randomNumber2 = getRandomNumber(0, 100);
+  const givenNumbers = `${randomNumber1} ${randomNumber2}`;
+  const correctAnswer = findsGcd(randomNumber1, randomNumber2);
+  return [givenNumbers, `${correctAnswer}`];
 };
 
 const startGame = () => {
